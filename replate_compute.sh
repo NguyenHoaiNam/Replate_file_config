47d37f8778345c2dc1abdbd5a23b750c23e4f84c
Nguyen Hoai Nam <namptit307@gmail.com>
2015-12-26 21:40:29 +0700

    Create replate_compute.sh

---
+++ replate_compute.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Scrips run on COMPUTE node"
+sleep 5
+echo "Enter VIP:"
+read VIP
+
+## With Neutron
+
+echo "Replate into file neutron.conf"
+sleep 3
+
+sudo cp /etc/neutron/neutron.conf /etc/neutron/neutron.conf.bka
+
+sudo sed -i "s/10.10.10.11:/$VIP:/g" /etc/neutron/neutron.conf
+
+sudo sed -i "s/rabbit_hosts = 10.10.10.11/rabbit_hosts = 10.10.10.11,10.10.10.12,10.10.10.13/g" \
+/etc/neutron/neutron.conf
+
+## With Nova
+
+echo "Replate into file nova.conf"
+sleep 3
+
+sudo cp /etc/nova/nova.conf /etc/nova/nova.conf.bka
+
+sudo sed -i "s/10.10.10.11:/$VIP:/g"  /etc/nova/nova.conf
+
+sudo sed -i "s/novncproxy_base_url = http:\/\/10.10.10.15:6080\/vnc_auto.html/\
+novncproxy_base_url = http:\/\/$VIP:6080\/vnc_auto.html/g"\
+/etc/nova/nova.conf
+
+sudo sed -i "s/rabbit_hosts = 10.10.10.11/rabbit_hosts = 10.10.10.11,10.10.10.12,10.10.10.13/g"\
+/etc/nova/nova.conf
+
+####
+
+echo "Check again when we have changed !!!"
+echo "File neutron.conf"
+diff /etc/neutron/neutron.conf /etc/neutron/neutron.conf.bka
+echo "File nova.conf"
+diff /etc/nova/nova.conf /etc/nova/nova.conf.bka
